70aa9d6565aa7be0d164713cbb93891c57cf2d87
Mariano Antonio Montano <montano.mariano@gmail.com>
2021-01-28 19:03:55 -0300

    Actualizaciones pendientes
    Actualizaciones pendientes

--- WEB Dolar/Controllers/DolarController.cs
+++ WEB Dolar/Controllers/DolarController.cs
@@ -3,18 +3,50 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using WEB_Dolar.Models;
 using WEB_Dolar.Services;
+using System.Linq;
+using System;
 
 namespace WEB_Dolar.Controllers
 {
     public class DolarController : Controller
-    {
+    { 
         // GET: Dolar
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(string ordenamiento, DateTime? inicio, DateTime? fin)
         {
             ApiHelper.InicializarCliente();
-            List<DolarModel> Cotizaciones = new List<DolarModel>();
-            Cotizaciones = await BCRA_Service.ListarTodos();
-            return View(Cotizaciones);
+            var cotizaciones =
+                from c in await BCRA_Service.ListarTodos()
+                select c;
+
+            ViewBag.OrdenPorFecha = string.IsNullOrEmpty(ordenamiento) ? "fecha_desc" : "";
+            ViewBag.OrdenPorValor = string.IsNullOrEmpty(ordenamiento) ? "valor_desc" : "";
+            
+            //Ordenamiento
+            if (ordenamiento == "fecha_desc")
+                cotizaciones = cotizaciones.OrderByDescending(x => x.D);
+
+            if (ordenamiento == "valor_desc")
+                cotizaciones = cotizaciones.OrderByDescending(x => x.V);
+
+
+            //Filtrado
+            DateTime _inicio = cotizaciones.OrderBy(x => x.D).Take(1).First().D;
+            DateTime _fin = cotizaciones.OrderByDescending(x => x.D).Take(1).First().D;
+
+            if (string.IsNullOrEmpty(inicio.ToString()) == false)
+            {
+                _inicio = inicio.Value;
+                cotizaciones = cotizaciones.Where(x => x.D >= _inicio).ToList();
+            }
+
+            if (string.IsNullOrWhiteSpace(fin.ToString()) == false)
+            {
+                _fin = fin.Value;
+                cotizaciones = cotizaciones.Where(x => x.D <= _fin).ToList();
+            }
+
+
+            return View(cotizaciones);
         }
 
         // GET: Dolar/Details/5
--- WEB Dolar/Models/DolarModel.cs
+++ WEB Dolar/Models/DolarModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Diagnostics;
 
 namespace WEB_Dolar.Models
 {
+    [DebuggerDisplay("Fecha: {D}")]
     public class DolarModel
     {
         [Display(Name ="Fecha")]
